a4c0f3b7f88c5e591931ca4fb0ce36596eb2e310
wickedev <orange881217@gmail.com>
2022-05-03 22:29:43 +0900

    solve part2 using reductions

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -6,9 +6,13 @@
                (slurp)
                (clojure.string/split-lines)))
 
+(use 'clojure.tools.trace)
+
 ;; 파트 1
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
+
+(def numbers  (map parse-long input))
 
 (defn parseInt [str-nums] (map  #(Integer/parseInt %) str-nums))
 
@@ -16,8 +20,7 @@
 ;; PPAP (parse, process, aggregate, print)
 ;; parse -> 알고리즘을 적용하기 전에 인풋값을 전처리하는 행위
 ;; process -> 문제를 풀기위한 알고리즘을 적용하는 행위
-(->> input
-     (map #(Integer/parseInt %))
+(->> numbers
      (reduce +))
 
 (reduce + (parseInt input))
@@ -39,32 +42,43 @@
     (if (contains? prev-results result)
       result
       (first-dupulicate-number rest-numbers result (if (nil? acc) prev-results (conj prev-results result))))))
+(comment
+  (first-dupulicate-number (cycle [+1, -1]) nil (set '()))
+  (first-dupulicate-number (cycle [+3, +3, +4, -2, -4]) nil (set '()))
+  (first-dupulicate-number (cycle [-6, +3, +8, +5, -6]) nil (set '()))
+  (first-dupulicate-number (cycle [+7, +7, -2, -7, -4]) nil (set '())))
 
-(first-dupulicate-number (cycle [+1, -1]) nil (set '()))
-(first-dupulicate-number (cycle [+3, +3, +4, -2, -4]) nil (set '()))
-(first-dupulicate-number (cycle [-6, +3, +8, +5, -6]) nil (set '()))
-(first-dupulicate-number (cycle [+7, +7, -2, -7, -4]) nil (set '()))
-
-; FIXME: Evalute 불가
-(defn first-dupulicate-number-with-loop
+(defn solve
   "numbers 앞에서부터 더하여 2번 이상 동일한 결과값을 내는 첫번째 결과를 반환"
-  [numbers ; 더해야 할 숫자의 목록
-   sum ; 값을 가산하기 위한  0
+  [sum ; 값을 가산하기 위한  0
    seen? ; 이전 합산 결과를 저장하기 위한 #{}
+   numbers ; 더해야 할 숫자의 목록
    ]
-   (if (seen? sum)
-      sum
-      (let [sum' (+ sum (first numbers))
-            seen?' (conj seen? sum)]
-            (recur sum' seen?' (next numbers)))))
-
+  (if (seen? sum)
+    sum
+    (let [sum' (+ sum (first numbers))
+          seen?' (conj seen? sum)]
+      (recur sum' seen?' (next numbers)))))
 
 (comment
- (first-dupulicate-number-with-loop (cycle [1, -1]) 0 #{})
-;;  (first-dupulicate-number-with-loop (cycle [+3, +3, +4, -2, -4]) 0 #{})
-;;  (first-dupulicate-number-with-loop (cycle [-6, +3, +8, +5, -6]) 0 #{})
-;;  (first-dupulicate-number-with-loop (cycle [+7, +7, -2, -7, -4]) 0 #{})
-;;  (first-dupulicate-number-with-loop (cycle (parseInt input)) nil (set '()))
-)
+  (->> input
+       (map parse-long)
+       cycle
+       (solve 0 #{})))
 
+(defn solve2 [numbers]
+  (->> numbers
+       cycle
+       (reductions +)
+       (reduce (fn [seen? sum]
+                 (if (seen? sum)
+                   (reduced sum)
+                   (conj seen? sum)))
+               #{0})))
 
+(comment
+  (solve2 [+1, -1])
+  (solve2 [+3, +3, +4, -2, -4])
+  (solve2 [-6, +3, +8, +5, -6])
+  (solve2 [+7, +7, -2, -7, -4])
+  (solve2 numbers))

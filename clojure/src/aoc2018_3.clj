4fa507d32c07771bea2d676c6ba02cf0ba4e03cd
wickedev <orange881217@gmail.com>
2022-05-06 16:31:28 +0900

    aoc 2018 part 1

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,5 +1,12 @@
-(ns aoc2018_3)
+(ns aoc2018_3
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.tools.trace :as trace :refer (trace)]))
 
+(def input (-> "day3.sample2.txt"
+               (io/resource)
+               (slurp)
+               (str/split-lines)))
 
 ;; 파트 1
 ;; 다음과 같은 입력이 주어짐.
@@ -23,6 +30,68 @@
 ;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
 ;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
 
+(def input-pattern #"#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)")
+
+(defn parse-input [s] (-> (->> s
+                               (re-find input-pattern)
+                               rest
+                               (zipmap [:ids :x :y :w :h]))
+                          (update :ids #(str/split % #""))
+                          (update :x read-string)
+                          (update :y read-string)
+                          (update :w read-string)
+                          (update :h read-string)))
+
+(comment
+  (parse-input "#1 @ 1,3: 4x4") ; {:ids ["1"], :x 1, :y 3, :w 4, :h 4} 
+  (parse-input "#11 @ 11,33: 44x44") ; {:id 11, :x 11, :y 33, :width 44, :height 44}
+  )
+
+(defn repeatedly-indexed [w f]
+  (map-indexed (fn [i _] (f i)) (range w)))
+
+(comment
+  (repeatedly-indexed 3 (fn [y] (repeatedly-indexed 3 (fn [x] [x y])))))
+
+(defn make-fabric
+  [{id :id x :x y :y w :w h :h}]
+  (let [width (+ x w)
+        height (+ y h)]
+    (repeatedly-indexed
+     height
+     (fn [y']
+       (repeatedly-indexed
+        width
+        (fn [x']
+          (cond
+            (and (<= x x')
+                 (<= y y')
+                 (<= x' width)
+                 (<= y' height)) id
+            :else \')))))))
+
+(defn make-fabrics
+  [options]
+  (->> options
+       (map (fn [{ids :ids x :x y :y w :w h :h}]
+              (->> ids
+                   (map #(make-fabric {:id % :x x :y y :w w :h h})))))))
+
+(comment
+  (make-fabric {:id "1" :x 1 :y 3 :w 4 :h 4}))
+
+(defn zip
+  "순차적으로 두 컬렉션을 묶습니다."
+  [coll1 coll2] (map vector coll1 coll2))
+
+(defn zip-fabric [fabric1 fabric2] fabric1)
+
+(comment
+  (->> ["#1 @ 1,3: 4x4"
+        "#2 @ 3,1: 4x4"
+        "#3 @ 5,5: 2x2"]
+       (map parse-input)
+       make-fabrics))
 
 
 

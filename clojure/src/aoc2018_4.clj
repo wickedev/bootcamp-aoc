c26a27bb6f6ba7bc914100ba462868204cce542c
wickedev <orange881217@gmail.com>
2022-05-10 02:34:14 +0900

    aoc 2018 day4 solve

--- clojure/src/aoc2018_4.clj
+++ clojure/src/aoc2018_4.clj
@@ -1,6 +1,6 @@
 (ns aoc2018-4
   (:require [utils :refer [read-resource]]
-            [clojure.core.match :refer [match]]))
+            [clojure.string :as string]))
 
 (def input (read-resource "aoc2018_4_sample.txt"))
 
@@ -33,163 +33,103 @@
 ;; 파트 1은 “주어진 입력에 대해서, 가장 오랜시간 잠들어있었던 가드의 ID와, 그 가드가 가장 빈번하게 잠들어 있었던 분(minute)의 곱을 구하라”
 ;; 만약 20번 가드가 0시 10분~36분, 다음날 0시 5분~11분, 다다음날 0시 11분~13분 이렇게 잠들어 있었다면, “11분“이 가장 빈번하게 잠들어 있던 ‘분’. 그럼 답은 20 * 11 = 220.
 
-(def record-pattern #"\[(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})\]\s(.*)")
+(def record-pattern #"\[\d{4}-\d{2}-\d{2} \d{2}:(\d{2})\]\s[\w]+ ?\#?(\d+)?.+")
 (def guard-shift-pattern #"Guard #(\d+) begins shift")
 
 (defn parse-record
-  "[1518-11-01 00:00] x 형식의 레코드를 {:year 0 :month 0 :day 0 :hour 0 :minute 0} 형식으로 파싱"
+  "[1518-11-01 00:00] x 형식의 레코드를 {log: \"\" :datetime {:year 0 :month 0 :day 0 :hour 0 :minute 0}} 형식으로 파싱"
   [input]
-  (let [[_ year month day hour minute log] (re-find record-pattern input)]
-    {:log log
-     :datetime {:year (read-string year)
-                :month (read-string month)
-                :day (read-string day)
-                :hour (read-string hour)
-                :minute (read-string minute)}}))
+  (let [[_ minute guard] (re-find record-pattern input)
+        shift? (not (nil? guard))]
+    {:minute (Integer/parseInt minute)
+     :shift? shift?
+     :guard (when shift? (read-string guard))}))
 
-(defn parse-id
-  "Guard #id begins shift 형식 로그에서 id를 파싱"
-  [log]
-  (let [matched (last (re-find guard-shift-pattern log))]
-    (when (seq matched) (read-string matched))))
 
-(comment
-  (parse-id "Guard #10 begins shift")
-  (parse-id "falls asleep"))
+(defn filling-guards
+  "raw records ({:minute 0, :shift? true, :guard 10} 
+   {:guard nil, :hour nil, :minute 5, :shift? false}) 중
+   guard가 nil 인 경우 직전 레코드 id로 채워 넣음"
+  [raw-records]
+  (reductions (fn [acc {:keys [guard shift? hour minute]}]
+                (let [id (:guard acc)]
+                  {:guard (or guard id) :hour hour :minute minute :shift? shift?}))
+              raw-records))
 
-(defn set-top
-  "컬렉션 마지막에 원소를 삽입한 새로운 컬랙션을 반환"
-  [coll x]
-  (conj (pop coll) x))
+(defn aggregate-records
+  "raw record ({:minute 0, :shift? true, :guard 10}...)를
+   {:guard 10, :minutes (0 5 25), :sleep-ranges ((5 25)), :sleep 20} 형식으로 반환"
+  [raw-record]
+  (println raw-record)
+  (let [sleep-ranges (->>  raw-record
+                           (filter (fn [x] (not (:shift? x))))
+                           (map :minute)
+                           (partition 2))]
+    {:guard (:guard (first raw-record)),
+     :minutes (map :minute raw-record)
+     :sleep-ranges sleep-ranges
+     :sleep (->>  sleep-ranges
+                  (map (partial apply -))
+                  (map abs)
+                  (apply +))}))
 
-(defn collapse-records
+(defn group-by-guard
+  "원본 레코드 ({:minute 0, :shift? true, :guard 10} ...) 를 guard 별로 묶음"
   [records]
-  (reduce (fn [acc {datetime :datetime , log :log}]
-            (let [id (parse-id log)]
-              #_(println :id id :datetime datetime :log log)
-              (match [log]
-                ["falls asleep"] (let [elem (last acc)]
-                                   (conj acc {:id (:id elem) :timestamp datetime}))
-                ["wakes up"] (let [elem (last acc)]
-                               (conj acc {:id (:id elem) :timestamp datetime}))
-                :else (conj acc {:id id
-                                 :timestamp datetime}))))
-          [] records))
+  (reduce (fn [acc {:keys [guard minutes sleep sleep-ranges]}]
+            (let [minutes' (get acc :minutes)
+                  sleep' (get acc :sleep)
+                  sleep-between' (get acc :sleep-ranges)]
+              {:guard guard
+               :minutes (concat minutes' minutes)
+               :sleep-ranges (concat sleep-between' sleep-ranges)
+               :sleep (+ sleep' sleep)})) records))
 
-(defn collapse-timelines
-  "id를 기준으로 하나의 맵으로 병합하고 :timeline 에 :timestamp를 모아 리스트로 반환 {:id 1 :timestap: ({...})}"
-  [timeline]
-  (let [timelines' (map :timestamp timeline)]
-    (reduce (fn [_ x]
-              {:id (:id x) :timelines timelines'})
-            {}
-            timeline)))
+(defn sleep-sumaries
 
-(defn datetime->minute
-  "{:year 0 :month 0 :day 0 :hour 0 :minute 0} 형식의 맵을 분으로 변환"
-  [{year :year, month :month, day :day, hour :hour, minute :minute}]
-  (+ (* year 525960)
-     (* month 43800)
-     (* day 1440)
-     (* hour 60)
-     minute))
+  [records]
+  (->> records
+       filling-guards
+       (partition-by :guard)
+       (map aggregate-records)
+       (group-by :guard)
+       vals
+       (map group-by-guard)))
 
-(comment
-  (datetime->minute {:year 1518, :month 11, :day 1, :hour 0, :minute 0}))
+(defn sleepiest-guard-in
+  "정리한 기록 ({:total-sleep ...}) 중에 가장 많이 잔 guard의 기록을 반환
+   {:guard 10, :minutes (0 5 25), :sleep-ranges ((5 25)), :sleep 20}"
+  [sumaries]
+  (println sumaries)
+  (->> sumaries
+       (sort-by :sleep >)
+       first))
 
-(defn calc-diff-minutes
-  "timelines 간에 diff 시간을 리스트로 반환"
-  [id timelines diff]
-  (let [curr (first timelines)
-        next (first (next timelines))]
-    (cond
-      (nil? next) diff
-      (seq timelines) (recur id (rest timelines) (conj diff (- next curr)))
-      :else nil)))
+(defn sleepiest-minute-in
+  "((5 25) (10 30)) 형식의 sleep minute 사이에
+   가장 많이 잔 minute을 뽑아서
+   [45 1] 처럼 first 에는 minute 두번째에는 빈도를 반환"
+  [sleep-ranges]
+  (->> sleep-ranges
+       (map #(range (first %) (second %)))
+       flatten
+       frequencies
+       (sort-by val >)
+       first))
 
-(comment
-  ;; 각 시간의 차이 만큼 출력 (25 5 20 5)
-  (calc-diff-minutes  10 '(798890520 798890525 798890545 798890550 798890575) '()))
-
-
-(defn diffs->wake-and-sleep
-  "diffs (25 5 20 5) 같이 사이의 갭을 짝수 인덱스는 wakes로 홀수는 sleeps로 합산하여 반환 {:wakes 45 :sleeps 10}"
-  [diffs]
-  (let [spitted-diffs (->> diffs
-                           (map-indexed vector)
-                           (group-by (fn [x] (odd? (first x))))
-                           vals
-                           (map (fn [x] (map last x))))
-        wakes (first spitted-diffs)
-        sleeps (last spitted-diffs)]
-    {:wakes (apply + wakes) :sleeps (apply + sleeps)}))
-
-;;; WIP
-(defn solve-4-1 [inputs]
-  (->> inputs
-       (map parse-record)
-       collapse-records
-       (partition-by :id)
-      ;;  (({:id 10, :timestamp {:year 1518, :month 11, :day 1, :hour 0, :minute 0}}
-      ;;    {:id 10, :timestamp {:year 1518, :month 11, :day 1, :hour 0, :minute 5}}
-      ;;    {:id 10, :timestamp {:year 1518, :month 11, :day 1, :hour 0, :minute 25}}
-      ;;    {:id 10, :timestamp {:year 1518, :month 11, :day 1, :hour 0, :minute 30}}
-      ;;    {:id 10, :timestamp {:year 1518, :month 11, :day 1, :hour 0, :minute 55}})
-      ;;   ({:id 99, :timestamp {:year 1518, :month 11, :day 1, :hour 23, :minute 58}}
-      ;;    {:id 99, :timestamp {:year 1518, :month 11, :day 2, :hour 0, :minute 40}}
-      ;;    {:id 99, :timestamp {:year 1518, :month 11, :day 2, :hour 0, :minute 50}})
-      ;;   ({:id 10, :timestamp {:year 1518, :month 11, :day 3, :hour 0, :minute 5}}
-      ;;    {:id 10, :timestamp {:year 1518, :month 11, :day 3, :hour 0, :minute 24}}
-      ;;    {:id 10, :timestamp {:year 1518, :month 11, :day 3, :hour 0, :minute 29}})
-      ;;   ({:id 99, :timestamp {:year 1518, :month 11, :day 4, :hour 0, :minute 2}}
-      ;;    {:id 99, :timestamp {:year 1518, :month 11, :day 4, :hour 0, :minute 36}}
-      ;;    {:id 99, :timestamp {:year 1518, :month 11, :day 4, :hour 0, :minute 46}}
-      ;;    {:id 99, :timestamp {:year 1518, :month 11, :day 5, :hour 0, :minute 3}}
-      ;;    {:id 99, :timestamp {:year 1518, :month 11, :day 5, :hour 0, :minute 45}}
-      ;;    {:id 99, :timestamp {:year 1518, :month 11, :day 5, :hour 0, :minute 55}}))
-       (map collapse-timelines)
-      ;;  ({:id 10,
-      ;;    :timelines
-      ;;    ({:year 1518, :month 11, :day 1, :hour 0, :minute 0}
-      ;;     {:year 1518, :month 11, :day 1, :hour 0, :minute 5}
-      ;;     {:year 1518, :month 11, :day 1, :hour 0, :minute 25}
-      ;;     {:year 1518, :month 11, :day 1, :hour 0, :minute 30}
-      ;;     {:year 1518, :month 11, :day 1, :hour 0, :minute 55})}
-      ;;   {:id 99,
-      ;;    :timelines
-      ;;    ({:year 1518, :month 11, :day 1, :hour 23, :minute 58}
-      ;;     {:year 1518, :month 11, :day 2, :hour 0, :minute 40}
-      ;;     {:year 1518, :month 11, :day 2, :hour 0, :minute 50})}
-      ;;   {:id 10,
-      ;;    :timelines
-      ;;    ({:year 1518, :month 11, :day 3, :hour 0, :minute 5}
-      ;;     {:year 1518, :month 11, :day 3, :hour 0, :minute 24}
-      ;;     {:year 1518, :month 11, :day 3, :hour 0, :minute 29})}
-      ;;   {:id 99,
-      ;;    :timelines
-      ;;    ({:year 1518, :month 11, :day 4, :hour 0, :minute 2}
-      ;;     {:year 1518, :month 11, :day 4, :hour 0, :minute 36}
-      ;;     {:year 1518, :month 11, :day 4, :hour 0, :minute 46}
-      ;;     {:year 1518, :month 11, :day 5, :hour 0, :minute 3}
-      ;;     {:year 1518, :month 11, :day 5, :hour 0, :minute 45}
-      ;;     {:year 1518, :month 11, :day 5, :hour 0, :minute 55})})
-       (map #(update % :timelines (fn [x] (map datetime->minute x))))
-      ;;  ({:id 10, :timelines (798890520 798890525 798890545 798890550 798890575)}
-      ;;   {:id 99, :timelines (798891958 798892000 798892010)}
-      ;;   {:id 10, :timelines (798893405 798893424 798893429)}
-      ;;   {:id 99, :timelines (798894842 798894876 798894886 798896283 798896325 798896335)})
-       (map (fn [{id :id, timelines :timelines}]
-              (let [diffs (calc-diff-minutes id timelines '())]
-                {:id id, :wakes-and-sleeps (diffs->wake-and-sleep diffs)})))))
-      ;; ({:id 10, :wakes-and-sleeps {:wakes 45, :sleeps 10}}
-      ;;  {:id 99, :wakes-and-sleeps {:wakes 10, :sleeps 42}}
-      ;;  {:id 10, :wakes-and-sleeps {:wakes 5, :sleeps 19}}
-      ;;  {:id 99, :wakes-and-sleeps {:wakes 1441, :sleeps 52}})
-
-;; diffs 를 인덱스로 분할하려고 함
+(defn solve-4-1-v2 [inputs]
+  (let [records (->> inputs
+                     sort
+                     (map parse-record))
+        sumaries (sleep-sumaries records)
+        sleepiest-guard (sleepiest-guard-in sumaries)
+        sleep-ranges (:sleep-ranges sleepiest-guard)
+        sleepiest-guard-id (:guard sleepiest-guard)
+        sleepiest-minute (first (sleepiest-minute-in sleep-ranges))]
+    (* sleepiest-guard-id sleepiest-minute)))
 
 (comment
-  (solve-4-1
+  (solve-4-1-v2
    ["[1518-11-01 00:00] Guard #10 begins shift"
     "[1518-11-01 00:05] falls asleep"
     "[1518-11-01 00:25] wakes up"
@@ -207,7 +147,7 @@
     "[1518-11-05 00:03] Guard #99 begins shift"
     "[1518-11-05 00:45] falls asleep"
     "[1518-11-05 00:55] wakes up"])
-  (solve-4-1 input))
+  (solve-4-1-v2 input))
 
 ;; 한타
 ;; Q1. 함수를 나누는 모범사례 단일 / 다중 항목
@@ -222,3 +162,55 @@
 ;; 파트 2
 ;; 주어진 분(minute)에 가장 많이 잠들어 있던 가드의 ID과 그 분(minute)을 곱한 값을 구하라.
 
+(defn freq-asleep-same-minute-guard
+  "동일한 시간에 잔 빈도가 가장 높은 가드를 반환"
+  [sumaries] (->> sumaries
+                  (map (fn [x]
+                         (let [freq (last (get x :sleepiest-minute  [0]))]
+                           [(if (nil? freq) 0 freq) x])))
+                  (sort-by first >)
+                  (map last)
+                  first))
+
+(defn with-sleepiest-minute
+  "기존 summaries에 가장 많이 잔 시간을 추가"
+  [summaries]
+  (map (fn [x]
+         (let [sleep-ranges (:sleep-ranges x)
+               sleepiest-minute (sleepiest-minute-in sleep-ranges)]
+           (assoc x :sleepiest-minute sleepiest-minute)))
+       summaries))
+
+(defn solve-4-2 [inputs]
+  (let [records (->> inputs
+                     sort
+                     (map parse-record))
+        sumaries (->> records
+                      sleep-sumaries
+                      with-sleepiest-minute)
+        freq-sleep-guard (freq-asleep-same-minute-guard sumaries)
+        sleep-ranges (:sleep-ranges freq-sleep-guard)
+        freq-sleep-guard-id (:guard freq-sleep-guard)
+        freq-sleep-minute (first (sleepiest-minute-in sleep-ranges))]
+    (* freq-sleep-guard-id freq-sleep-minute)))
+
+(comment
+  (solve-4-2
+   ["[1518-11-01 00:00] Guard #10 begins shift"
+    "[1518-11-01 00:05] falls asleep"
+    "[1518-11-01 00:25] wakes up"
+    "[1518-11-01 00:30] falls asleep"
+    "[1518-11-01 00:55] wakes up"
+    "[1518-11-01 23:58] Guaræd #99 begins shift"
+    "[1518-11-02 00:40] falls asleep"
+    "[1518-11-02 00:50] wakes up"
+    "[1518-11-03 00:05] Guard #10 begins shift"
+    "[1518-11-03 00:24] falls asleep"
+    "[1518-11-03 00:29] wakes up"
+    "[1518-11-04 00:02] Guard #99 begins shift"
+    "[1518-11-04 00:36] falls asleep"
+    "[1518-11-04 00:46] wakes up"
+    "[1518-11-05 00:03] Guard #99 begins shift"
+    "[1518-11-05 00:45] falls asleep"
+    "[1518-11-05 00:55] wakes up"])
+  (solve-4-2 input))

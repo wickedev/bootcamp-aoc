8995b8a54f56ed4ce783ed24fbb0b0eae063a8cb
wickedev <orange881217@gmail.com>
2022-05-10 11:20:04 +0900

    AOC 2018 Day5 Part1

--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -1,4 +1,9 @@
-(ns aoc2018_5)
+(ns aoc2018_5
+  (:require [utils :refer [read-resource]]))
+
+(def input (read-resource "aoc2018_5_sample.txt"))
+
+(def polymers (first input))
 ;; 파트 1
 ;; 입력: dabAcCaCBAcCcaDA
 
@@ -9,6 +14,41 @@
 ;; 예시 dabAcCaCBAcCcaDA => dabCBAcaDA
 
 ;; 주어진 input 에서 최종으로 남는 문자열을 리턴하시오.
+
+(defn polarity?
+  "2개의 문자를 인자로 받아 이 둘이 극성을 가지는지 판단한다
+   a a 혹은 a b 는 false, a A 혹은 A a 는 true"
+  [c1 c2]
+  (let [c1' (int c1)
+        c2' (int c2)]
+    (or (= c1' (+ c2' 32)) (= c1' (- c2' 32)))))
+
+(defn react-single-cycle
+  "한 사이클에 폴리머들이 반응하여 파괴되고 남은 폴리머들을 반환.
+   polymers abBA 같은 문자열이 주어지면 '(aA)를 반환"
+  [acc curr-unit]
+  (let [prev-unit (last acc)]
+    (cond
+      (nil? prev-unit) (conj acc curr-unit)
+      (polarity? prev-unit curr-unit) (pop acc)
+      :else (conj acc curr-unit))))
+
+(defn chain-react
+  "폴리머들이 연쇄적으로 반응하여 파괴되고 남은 폴리머들은 반환
+   polymers abBA 같은 문자열이 주어지면 '()를 반환"
+  [polymers]
+  (let [polyers' (reduce react-single-cycle [] polymers)]
+    (if (= (count polymers) (count polyers'))
+      polyers'
+      (recur polyers'))))
+
+(defn solve-5-1
+  [polymers]
+  (count (chain-react polymers)))
+
+(comment
+  (solve-5-1 "dabAcCaCBAcCcaDA")
+  (solve-5-1 polymers))
 
 ;; 파트 2
 ;; 주어진 문자열에서 한 유닛 (대문자와 소문자)을 전부 없앤 후 반응시켰을 때, 가장 짧은 문자열의 길이를 리턴하시오.

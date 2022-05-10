48d0aeea505cf55babd260819dd663b25899f0b0
wickedev <orange881217@gmail.com>
2022-05-10 14:47:26 +0900

    solve AoC 2018 Day5 Part2

--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -1,5 +1,6 @@
 (ns aoc2018_5
-  (:require [utils :refer [read-resource]]))
+  (:require [utils :refer [read-resource]]
+            [clojure.string :refer [upper-case]]))
 
 (def input (read-resource "aoc2018_5_sample.txt"))
 
@@ -20,20 +21,24 @@
    a a 혹은 a b 는 false, a A 혹은 A a 는 true"
   [unit1 unit2]
   (let [unit1' (int unit1)
-        unit2' (int unit2)
-        upper (- unit2' 32)
-        lower (+ unit2' 32)]
-    (or (= unit1' upper) (= unit1' lower))))
+        unit2' (int unit2)]
+    (= 32 (abs (- unit1' unit2')))))
+
+;; abs를 사용할 수 있다 (두 값의 차이와 32를 비교할 때)
+;; upperCase, lowerCase 함수를 사용할 수 있을것 같다
 
 (defn react-single-cycle
   "한 사이클에 폴리머들이 반응하여 파괴되고 남은 폴리머들을 반환.
    polymers abBA 같은 문자열이 주어지면 '(aA)를 반환"
   [acc curr-unit]
-  (let [prev-unit (last acc)]
+  (let [prev-unit (peek acc)]
     (cond
       (nil? prev-unit) (conj acc curr-unit)
       (polarity? prev-unit curr-unit) (pop acc)
       :else (conj acc curr-unit))))
+
+;; last O(log32 n), peek O(1), pop O(1)
+;; https://stackoverflow.com/questions/17048076/big-o-of-clojure-library-functions
 
 (defn chain-react
   "폴리머들이 연쇄적으로 반응하여 파괴되고 남은 폴리머들은 반환
@@ -56,3 +61,31 @@
 ;; 주어진 문자열에서 한 유닛 (대문자와 소문자)을 전부 없앤 후 반응시켰을 때, 가장 짧은 문자열의 길이를 리턴하시오.
 ;; 예를 들어 dabAcCaCBAcCcaDA 에서 a/A를 없애고 모두 반응시키면 dbCBcD가 되고 길이는 6인데 비해,
 ;; 같은 문자열에서 c/C를 없애고 모두 반응시키면 daDA가 남고 길이가 4이므로 4가 가장 짧은 길이가 됨.
+
+(defn char-range [start end] (range (int start) (int end)))
+
+(defn unit-filter
+  "폴리머에서 소문자 c의 소문자 혹은 대문자를 제외한 나머지 폴리머를 반환한다
+   입력이 :polymers \"abc\" :c 'c' 라면 \"ab\" 를 출력한다"
+  [polymers c]
+  (letfn [(case-insensitive-matched? [unit]
+            (let [unit' (int unit)
+                  exact-match (= c unit')
+                  upper-match (= 32 (- c unit'))]
+              (not (or exact-match upper-match))))]
+    (->> polymers
+         (filter case-insensitive-matched?)
+         (apply str))))
+
+(defn solve-5-2
+  [polymers]
+  (->> (char-range \a \z)
+       (map (partial unit-filter polymers))
+       distinct
+       (map chain-react)
+       (map count)
+       (apply min)))
+
+(comment
+  (solve-5-2 "dabAcCaCBAcCcaDA")
+  (solve-5-2 polymers))
